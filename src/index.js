cea0e7fa0275baf5b4f96ec9ff90a0cb9a4475ef
Karantechie <karankpguinfo001018@gmail.com>
2025-07-12 17:08:27 +0530

    connected to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,52 @@
+// require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+import { DB_NAME } from './constants';
+const app = express();
+
+(async() => {
+    try{
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on('error', (err) => {
+            console.error("Server error:", err);
+            throw err;
+        });
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    }
+    catch(error){
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+})()
+// apply ";" at the start of the line as it is a professional coding practice
+
+*/
